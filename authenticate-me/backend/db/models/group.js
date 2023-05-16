4143a3ffdcddd462e8c26be55adaabde94e1a06a
JonathanSCarter <jscarter3874112@gmail.com>
2023-05-15 22:56:17 -0400

    Get all spots

---
+++ authenticate-me/backend/db/models/group.js
@@ -0,0 +1,54 @@
+'use strict';
+const { error } = require('console');
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Group extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Group.belongsTo(models.User, {foreignKey: 'UserId'})
+    }
+  }
+  Group.init({
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(60),
+      allowNull: false
+    },
+    about: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isLong(value){
+          if(value.length < 50) throw new Error()
+        }
+      }
+    },
+    type: {
+      type: DataTypes.ENUM('Online', 'In person'),
+      allowNull: false
+    },
+    private: DataTypes.BOOLEAN,
+    city: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+  }, {
+    sequelize,
+    modelName: 'Group',
+  });
+  return Group;
+};
---
+++ authenticate-me/backend/db/seeders/20230516015722-demo-group.js
@@ -0,0 +1,65 @@
+'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    options.tableName = 'Groups';
+    return queryInterface.bulkInsert(options, [
+      {
+        UserId: 1,
+        name: 'Online Group 1',
+        about: 'This is an online group for people interested in hiking.',
+        type: 'Online',
+        private: false,
+        city: 'Los Angeles',
+        state: 'California',
+      },
+      {
+        UserId: 2,
+        name: 'In Person Group 1',
+        about: 'This is an in-person group for people interested in cooking.',
+        type: 'In person',
+        private: true,
+        city: 'San Francisco',
+        state: 'California'
+      },
+      {
+        UserId: 3,
+        name: 'Online Group 2',
+        about: 'This is an online group for people interested in yoga.',
+        type: 'Online',
+        private: true,
+        city: 'New York',
+        state: 'New York',
+      }
+    ], {});
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    options.tableName = 'Groups';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      name: { [Op.in]: ['Online Group 1', 'In Person Group 1', 'Online Group 2'] }
+    }, {});
+  }
+};
