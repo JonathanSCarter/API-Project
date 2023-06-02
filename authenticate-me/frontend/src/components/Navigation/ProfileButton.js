5ae994d5a0898188c72a5d124dcf5e7b03359ad5
JonathanSCarter <jscarter3874112@gmail.com>
2023-06-02 15:53:40 -0400

    nav done

---
+++ authenticate-me/frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,9 @@
+const Meat = () => {
+  return (
+    <div style={{ color: "brown", fontSize: "100px" }}>
+      <i class="fa-solid fa-drumstick-bite"></i>
+    </div>
+  );
+};
+
+export default Meat
